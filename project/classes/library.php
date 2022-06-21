07762a386086aa70959f6c5daf908c8543021d85
yeeda <a0905990165@gmail.com>
2022-06-21 18:19:16 +0800

    花最多錢

--- project/classes/library.php
+++ project/classes/library.php
@@ -1,13 +1,13 @@
 <?php
 $sql = "SELECT truck_delivery.truck_id, delivery_order.order_id, customer.customer_name
-FROM truck_delivery
-LEFT JOIN delivery_order ON truck_delivery.delivery_id = delivery_order.delivery_id
-LEFT JOIN order_product ON delivery_order.order_id = order_product.order_id
-LEFT JOIN product_customer ON order_product.product_id = product_customer.product_id
-LEFT JOIN customer ON product_customer.customer_id = customer.customer_id
-WHERE truck_delivery.truck_id = '1721'
-GROUP BY customer.customer_name
-";       //1721車上貨物的customer
+        FROM truck_delivery
+        LEFT JOIN delivery_order ON truck_delivery.delivery_id = delivery_order.delivery_id
+        LEFT JOIN order_product ON delivery_order.order_id = order_product.order_id
+        LEFT JOIN product_customer ON order_product.product_id = product_customer.product_id
+        LEFT JOIN customer ON product_customer.customer_id = customer.customer_id
+        WHERE truck_delivery.truck_id = '1721'
+        GROUP BY customer.customer_name
+";       //1721車上貨物的customer OK
 
 $sql = "SELECT order_product.order_id,product.product_name 
         FROM truck LEFT JOIN truck_delivery ON truck.truck_id = truck_delivery.truck_id 
@@ -15,8 +15,9 @@ $sql = "SELECT order_product.order_id,product.product_name
         LEFT JOIN delivery_order ON delivery.delivery_id = delivery_order.delivery_id
         LEFT JOIN order_product ON delivery_order.order_id = order_product.order_id 
         LEFT JOIN product ON order_product.product_id = product.product_id
-        WHERE truck.truck_id = '1721' AND delivery.success_number = truck.last_success
-";       //1721上一次成功的貨物
+        WHERE truck.truck_id = '1721' 
+        AND delivery.success_number = truck.last_success
+";       //1721上一次成功的貨物 OK
 
 $sql = "SELECT product_customer.customer_id, product_customer,customer_name , MAX(SUM(ship_fee)) as fee 
         FROM order_product
@@ -24,6 +25,17 @@ $sql = "SELECT product_customer.customer_id, product_customer,customer_name , MA
         LEFT JOIN product_customer ON order_product.product_id = product_customer.product_id
         group by product_customer.customer_id
         WHERE orderlist.timeline BETWEEN 'Jan-01-2022' AND 'Dec-31-2022';
+";       //運費最多
+
+$sql = "SELECT MAX(
+                SELECT customer.customer_id, customer.customer_name, SUM(orderlist.ship_fee) AS Fee
+                FROM orderlist
+                LEFT JOIN order_product ON orderlist.order_id = order_product.order_id 
+                LEFT JOIN product_customer ON order_product.product_id = product_customer.product_id
+                LEFT JOIN customer ON product_customer.customer_id = customer.customer_id
+                GROUP BY customer.customer_id
+                ORDER BY Fee DESC
+                )
 ";       //運費最多
 
 $sql = "SELECT product_customer.customer_id, product_customer,customer_name , MAX(count(order_product.order_id))
@@ -40,5 +52,52 @@ $sql = "SELECT order_product.order_id, order_product.product_id
         LEFT JOIN delivery ON delivery_order.delivery_id = delivery.delivery_id
         LEFT JOIN orderlist ON delivery_order.order_id = orderlist.order_id
         WHERE orderlist.timeline < delivery.arrival 
-";       //時間不對
-//soutcetree 注意一點
+";       //時間不對OK
+
+
+// SELECT ('truck') AS name, COUNT(truck_id) AS count數量
+// FROM truck
+
+// UNION
+// SELECT ('truck_delivery') AS name, COUNT(truck_id)
+// FROM truck_delivery
+
+// UNION
+// SELECT ('delivery') AS name, COUNT(delivery_id)
+// FROM delivery
+
+// UNION
+// SELECT ('delivery_order') AS name, COUNT(delivery_id)
+// FROM delivery_order
+
+// UNION
+// SELECT ('orderlist') AS name, COUNT(order_id)
+// FROM orderlist
+
+// UNION
+// SELECT ('order_product') AS name, COUNT(order_id)
+// FROM order_product
+
+// UNION
+// SELECT ('product') AS name, COUNT(product_id)
+// FROM product
+
+// UNION
+// SELECT ('product_customer') AS name, COUNT(product_id)
+// FROM product_customer
+
+// UNION
+// SELECT ('customer') AS name, COUNT(customer_id)
+// FROM customer
+
+// UNION
+// SELECT ('sent_customer') AS name, COUNT(sent_id)
+// FROM sent_customer
+
+// UNION
+// SELECT ('order_sent') AS name, COUNT(order_id)
+// FROM order_sent
+
+// UNION
+// SELECT ('sent') AS name, COUNT(sent_id)
+// FROM sent
